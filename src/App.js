a919a81d787a71169e9fea33f98deda92a3025d0
Casey Wight <casey@chapterthree.com>
2017-02-11 17:49:50 -0800

    More better.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,55 @@ class App extends Component {
   constructor() {
     super();
     this.addCounter = this.addCounter.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    // this.decrement = this.decrement.bind(this);
     this.state = {
-      counters: {}
+      counters: [
+        { key: 0, value: 34 },
+        { key: 1, value: 45 },
+        { key: 2, value: 56 },
+      ]
     };
   }
 
-  addCounter(counter, value) {
-    const nextState = this.state.counters;
-    const timestamp = Date.now();
-    nextState[`counter-${timestamp}`] = counter;
-    this.setState({ nextState }); // set state
+  addCounter(value) {
+    let counter = { value: value }        // Data for the new counter
+    const counters = this.state.counters; // is an array
+    counters.push(counter);               // add the new counter to the Counters array
+    this.setState({ counters }); // set state of Counters to be the updated / new Counters
+    // const nextState = { counters: [{...counters}, {counter}]};
+    //   console.log('nextState', nextState);
+    //   console.log('state', this.state);
+    // this.setState({ nextState }); // set state
+    // console.log('addCounter nextState', nextState);
   }
 
   addValue(amount) {
+    const counters = this.state.counters;
+    counters.map((counter) => counter.value = 40);
+    this.setState({ counters });
+  }
+
+  handleChange(key, dir) {
+    console.log('key', key);
+    const counters = this.state.counters;
+    const counter = counters[key];
+    if (dir == 'up') {
+      counter.value++;
+    } else {
+      counter.value--;
+    }
+    this.setState({ counters });
+  }
+
+  decrement() {
+    const nextState = { ...this.state };
+    nextState.counter.value = this.state.counter.value - 1;
+    this.setState(nextState);
   }
 
   componentWillMount() {
-    this.addCounter({value: 0});
+    // this.addCounter();
   }
 
   render() {
@@ -32,13 +64,17 @@ class App extends Component {
           <h2>HORSE.PARTS</h2>
         </div>
         <div className="config">
-          <button onClick={this.addCounter}>Add Counter</button>
-          <button onClick={this.addValue(40)}>Add Value</button>
+          <button onClick={() => this.addCounter(22)}>Add Counter</button>
+          <button onClick={() => this.addValue(40)}>EDH</button>
         </div>
         <div className="counters">
-          {
-            Object.keys(this.state.counters)
-              .map(key => <Counter key={key} index={key} value="0" increment={this.increment} decrement={this.decrement} />)
+          {this.state.counters.map(
+            (counter) => // For each of the items in this "counters" array,
+            <Counter
+              key={counter.key}
+              value={counter.value}
+              increment={() => this.handleChange(counter.key, 'up')}
+              decrement={() => this.handleChange(counter.key, 'down')} />)
           }
         </div>
       </div>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.state = {
-      counter: {
-        value: 0
-      }
-    }
-  }
-
-  increment() {
-    const nextState = { ...this.state };
-    nextState.counter.value = this.state.counter.value + 1;
-    this.setState(nextState);
-  }
-
-  decrement() {
-    const nextState = { ...this.state };
-    nextState.counter.value = this.state.counter.value - 1;
-    this.setState(nextState);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.increment = this.increment.bind(this);
+  //   this.decrement = this.decrement.bind(this);
+  //   this.state = {
+  //     counter: {
+  //       value: 0
+  //     }
+  //   }
+  // }
 
   render() {
     return (
       <div className="counter">
-        <div className="tally">{this.state.counter.value}</div>
-        <button className="plus fa fa-plus" onClick={this.increment}></button>
-        <button className="minus fa fa-minus" onClick={this.decrement}></button>
+        <div className="tally">{this.props.value}</div>
+        <button className="plus fa fa-plus" onClick={this.props.increment}></button>
+        <button className="minus fa fa-minus" onClick={this.props.decrement}></button>
       </div>
     );
   }
